00d4c293e40ad4542a60cb73e7731840fe0a209c
Amaan Zahid <c1028030@hallam.shu.ac.uk>
2026-04-14 00:25:40 +0100

    update

--- furniture/urls.py
+++ furniture/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.create_furniture, name='create'),
+    path('list/', views.furniture_list, name='list'),
 ]
--- furniture/views.py
+++ furniture/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'furniture/home.html')
+
+def create_furniture(request):
+    return render(request, 'furniture/create.html')
+
+def furniture_list(request):
+    return render(request, 'furniture/list.html')
