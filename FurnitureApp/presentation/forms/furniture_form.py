b2aba9702c177db2e9178ab32e514bbd930280c4
Amaan Zahid <c1028030@hallam.shu.ac.uk>
2026-05-13 23:12:37 +0100

    file clean up

--- FurnitureApp/presentation/forms/furniture_form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from FurnitureApp.data.models.furniture import Furniture
-
-class Furnitureform(forms.ModelForm):
-    class Meta:
-        model = Furniture
-        fields = ["name", "wood_type", "price", "dimensions", "in_stock"]
